dfb7adac18cf23d3e97892aec1f59f4d875d77ec
Wyatt DeRamus <deramus.wyatt@gmail.com>
2023-04-17 17:25:00 -0500

    first commit

---
+++ src/main/java/com/wyatt/wyattspring/WyattSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wyatt.wyattspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyattSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyattSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyatt/wyattspring/WyattSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyatt.wyattspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyattSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
